40b940d64355d2fd38351ddc22f153a0c06cc628
Pol Piella Abadia <pol@appdiggershq.com>
2025-01-06 11:58:37 +0100

    Updates placeholder

--- src/components/ask-a-question/form.tsx
+++ src/components/ask-a-question/form.tsx
@@ -209,7 +209,7 @@ export const AskAQuestionForm: React.FC = () => {
                   type="text"
                   id="email-address-icon"
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-purple-500 focus:border-purple-500 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-purple-500 dark:focus:border-purple-500"
-                  placeholder="name@flowbite.com"
+                  placeholder="name@email.com"
                 />
               </div>
               {errors.email && (
